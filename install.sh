ecb8ca20d3b7049f5694db65ecd7f01bd81b09d2
Joel <joel.smith@alpsalpine.com>
2024-08-12 13:52:54 +0200

    updated nvim config

--- install.sh
+++ install.sh
@@ -9,7 +9,8 @@ fi
 
 # Install necessary packages (adjust for your package manager)
 $SUDO apt update
-$SUDO DEBIAN_FRONTEND=noninteractive apt install -y tzdata neovim tmux zsh ripgrep git curl neofetch
+
+$SUDO apt install -y tzdata neovim tmux zsh ripgrep git curl neofetch wget unzip fontconfig build-essential
 
 # Install Oh My Zsh (optional)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -18,9 +19,23 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 #git clone https://github.com/drlipton/dotfiles
 
 # Copy configuration files
-cp nvim ~/.config/nvim
+mkdir -p ~/.config/nvim
+
+cp -r nvim ~/.config/
 cp tmux/tmux.conf ~/.tmux.conf
+cp tmux/tmux.conf.local ~/.tmux.conf.local
 cp zsh/zshrc ~/.zshrc
+
+# Install Nerd Font (replace with your desired font)
+FONT_NAME="FiraCode Nerd Font"                                                           # Or another Nerd Font name
+FONT_URL="https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/FiraCode.zip" # Update URL if needed
+
+wget -O /tmp/nerdfont.zip $FONT_URL
+unzip /tmp/nerdfont.zip -d /tmp/nerdfont
+mkdir -p ~/.local/share/fonts
+cp /tmp/nerdfont/*.ttf ~/.local/share/fonts/
+cp /tmp/nerdfont/FiraCode-Regular Nerd Font Complete.ttf ~/.local/share/fonts/
+fc-cache -fv
 
 # Set Zsh as default shell (optional)
 chsh -s $(which zsh)
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,13 +2,17 @@
 
 return {
   "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = "maintained",
-      highlight = {
-        enable = true,
-      },
-    })
+  run = ":TSUpdate",
+  opts = {
+    ensure_installed = {
+      "bash", "c", "javascript", "lua", "python", "rust", "typescript", -- Add the languages you need
+    },
+    highlight = {
+      enable = true, -- false will disable the whole extension
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
   end,
 }
+
