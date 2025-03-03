9e32a3df936417a71df1588b55753712ff2a2c2c
joel smith <joel@joel.com>
2025-03-03 11:41:21 +0100

    removed telescope

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,11 +7,11 @@ vim.keymap.set("n", "<S-j>", ":enew<CR>", { silent = true })
 vim.keymap.set("n", "<S-k>", ":bdelete<CR>", { silent = true })
 
 -- Telescope
-vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", opts)
-vim.keymap.set("n", "<leader><space>", ":Telescope find_files<CR>", opts)
-vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
-vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", opts)
-vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>", opts)
+--vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+--vim.keymap.set("n", "<leader><space>", ":Telescope find_files<CR>", opts)
+--vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>", opts)
+--vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>", opts)
+--vim.keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>", opts)
 
 -- Terminal
 -- Open a terminal in a new horizontal split
